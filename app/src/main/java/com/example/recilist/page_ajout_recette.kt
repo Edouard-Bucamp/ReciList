125fa52c0e8c2f77adc475bc400c701ae4683cc7
Alexis-pdvn <alexispodevin1@gmail.com>
2021-01-18 21:40:03 +0100

    Création de la page ajout de recette + ajout des contraints sur les éléments de la page recette

---
+++ app/src/main/java/com/example/recilist/page_ajout_recette.kt
@@ -0,0 +1,11 @@
+package com.example.recilist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class page_ajout_recette : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_ajout_recette)
+    }
+}
