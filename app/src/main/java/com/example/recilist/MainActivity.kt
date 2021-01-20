73b75840e165f6f92ac8355dc3423e104c20fab8
Alexis-pdvn <alexispodevin1@gmail.com>
2021-01-20 10:55:36 +0100

    passage page recette a la recette cliquer

--- app/src/main/java/com/example/recilist/MainActivity.kt
+++ app/src/main/java/com/example/recilist/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.recilist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun Onclick(view: View) {
+        val intent = Intent (this, PageRecette::class.java);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/recilist/Page_recette.kt
+++ app/src/main/java/com/example/recilist/Page_recette.kt
@@ -3,7 +3,7 @@ package com.example.recilist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Page_recette : AppCompatActivity() {
+class PageRecette : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_recette)
