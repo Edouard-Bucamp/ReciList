8798e2eb698bd02b58b9a87b161d23adfb2f2582
Alexis-pdvn <alexispodevin1@gmail.com>
2021-01-20 11:56:03 +0100

    Ajout de la fonction onclick sur la page recette vers une recette

--- app/src/main/java/com/example/recilist/MainActivity.kt
+++ app/src/main/java/com/example/recilist/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.recilist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun Onclick(view: View) {
+        val intent = Intent (this, PageRecette::class.java);
+        startActivity(intent);
     }
 
 }
