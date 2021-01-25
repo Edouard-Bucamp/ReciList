81f1ad879c5506f4816261a67eb4f3f790b6ce6a
Alexis-pdvn <alexispodevin1@gmail.com>
2021-01-25 17:22:58 +0100

    insertion des ingrédients

---
+++ app/src/main/java/com/example/recilist/list_ingredients.kt
@@ -0,0 +1,50 @@
+package com.example.recilist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class list_ingredients(private val dataSet: List<String>,
+                       private val list_ingredients_layout: Int,
+                       private val onItemClickListener: ((Int) -> Unit)?,
+                       private val onItemLongClickListener: ((Int) -> Boolean)?):
+    RecyclerView.Adapter<list_ingredients.ViewHolder>() {
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(list_ingredients_layout, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text =dataSet[position]
+
+        // Set listeners on item (?.let content is executed only when listener is not null)
+        onItemClickListener?.let {
+            viewHolder.itemView.setOnClickListener { it(position) }
+        }
+
+        onItemLongClickListener?.let {
+            viewHolder.itemView.setOnLongClickListener { it(position) }
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+}
--- app/src/main/java/com/example/recilist/page_ajout_recette.kt
+++ app/src/main/java/com/example/recilist/page_ajout_recette.kt
@@ -3,12 +3,21 @@ package com.example.recilist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.bottom_navigation
+import kotlinx.android.synthetic.main.activity_page_ajout_recette.*
 
-class page_ajout_recette : AppCompatActivity() {
+class page_ajout_recette : AppCompatActivity(), TextView.OnEditorActionListener {
+    val list = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_ajout_recette)
+
 
         bottom_navigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
@@ -20,5 +29,26 @@ class page_ajout_recette : AppCompatActivity() {
                 else -> false
             }
         }
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = list_ingredients(list, R.layout.list_ingredients, null, this::removeAt)
+        editText.setOnEditorActionListener(this)
     }
+
+    override fun onEditorAction(textView: TextView?, actionId: Int, keyEvent: KeyEvent?): Boolean {
+        if(actionId == EditorInfo.IME_ACTION_DONE){ // Si on a validé le texte saisi
+            list.add(editText.text.toString()) // On ajoute le texte àla liste
+                    recyclerView.adapter?.notifyDataSetChanged() // On prévientque la liste a changé et doit être réaffichée
+            editText.text.clear(); // On efface le texte, pour faire dela place pour le prochain élément
+                    return true;
+        }
+        return false;
+    }
+
+    private fun removeAt(pos: Int): Boolean {
+        list.removeAt(pos)
+        recyclerView.adapter?.notifyDataSetChanged()
+        return true
+    }
+
 }
